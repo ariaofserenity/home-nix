27356b7a1e5fca602a4a30fb16612c64474ca1b9
aria <2664476+ariaofserenity@users.noreply.github.com>
2026-02-12 21:03:19 -0500

    hsr fixes

--- home/programs/games/hsr.nix
+++ home/programs/games/hsr.nix
@@ -2,15 +2,10 @@
   inputs,
   ...
 }:
-let
-  aagl-gtk-on-nix = import (
-    builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz"
-  );
-in
 {
 
   imports = [
-    aagl-gtk-on-nix.module
+     inputs.aagl.nixosModules.default
   ];
 
   programs.the-honkers-railway-launcher.enable = true;
