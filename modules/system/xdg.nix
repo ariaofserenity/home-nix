212448d1ab5702110253ca7947d331503f8db79e
aria <2664476+ariaofserenity@users.noreply.github.com>
2026-02-13 10:22:21 -0500

    gtk portal config

--- modules/system/xdg.nix
+++ modules/system/xdg.nix
@@ -2,9 +2,17 @@
   pkgs,
   ...
 }:
-
 {
   xdg.portal = {
     enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+    config = {
+      common = {
+        default = [ "gtk" ];
+      };
+    };
   };
 }
