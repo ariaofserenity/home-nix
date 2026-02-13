7f017a4ec27c87d325590f72391675a1379936bf
aria <2664476+ariaofserenity@users.noreply.github.com>
2026-02-13 10:01:23 -0500

    rdp to niri

--- modules/system/services/rdp.nix
+++ modules/system/services/rdp.nix
@@ -1,11 +1,10 @@
 { pkgs, ... }:
-
 {
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xrdp = {
+    enable = true;
+    defaultWindowManager = "${pkgs.niri}/bin/niri-session";
+    openFirewall = true;
+  };
 
-  services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "startplasma-x11";
-  services.xrdp.openFirewall = true;
+  services.displayManager.sessionPackages = [ pkgs.niri ];
 }
