df645243b13307046604848e52353fd0dd63190b
aria <2664476+ariaofserenity@users.noreply.github.com>
2026-02-12 20:56:13 -0500

    fixed zsh again

--- modules/system/shell/zsh.nix
+++ modules/system/shell/zsh.nix
@@ -5,27 +5,14 @@
 {
   programs.zsh = {
     enable = true;
-
     enableCompletion = true;
-    autosuggestions.enable = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    oh-my-zsh = {
-      enable = true;
-      theme = "bira";
-      plugins = [
-        "git"
-        "sudo"
-      ];
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
     };
-
-    initContent = ''
-      # Sets `nix develop` to use zsh
-      eval "$(nix-your-shell zsh)"
-
-      if [[ $- == *i* ]] && [[ -z $IN_NIX_SHELL ]]; then
-        fastfetch
-      fi
-    '';
+    history.size = 10000;
   };
 }
