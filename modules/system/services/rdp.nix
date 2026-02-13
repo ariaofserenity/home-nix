8d3fc81a22068b6bb9e1d2447dcb16a2bf387d76
aria <2664476+ariaofserenity@users.noreply.github.com>
2026-02-13 10:03:33 -0500

    try vnc instead

--- modules/system/services/rdp.nix
+++ modules/system/services/rdp.nix
@@ -1,10 +1,18 @@
 { pkgs, ... }:
 {
-  services.xrdp = {
-    enable = true;
-    defaultWindowManager = "${pkgs.niri}/bin/niri-session";
-    openFirewall = true;
+  environment.systemPackages = with pkgs; [
+    wayvnc
+  ];
+  
+  networking.firewall.allowedTCPPorts = [ 5900 ];
+  
+  systemd.user.services.wayvnc = {
+    description = "wayvnc VNC server";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.wayvnc}/bin/wayvnc 0.0.0.0 5900";
+      Restart = "on-failure";
+    };
   };
-
-  services.displayManager.sessionPackages = [ pkgs.niri ];
 }
