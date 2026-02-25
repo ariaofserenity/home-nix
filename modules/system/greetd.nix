2e325a8b609fd7fcc7523b152a2249dcc26cffa3
aria <ariaserenityvt@gmail.com>
2026-02-25 01:48:28 -0500

    removed unnecessary comments

--- modules/system/greetd.nix
+++ modules/system/greetd.nix
@@ -17,13 +17,7 @@ in
     };
   };
 
-  # this is a life saver.
-  # literally no documentation about this anywhere.
-  # might be good to write about this...
-  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
-
   systemd = {
-    # To prevent getting stuck at shutdown
     settings = {
       Manager = {
         DefaultTimeoutStopSec = "10s";
