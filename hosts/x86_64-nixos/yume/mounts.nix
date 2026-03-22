6932b7fd098ec275901f7a9e9c2d45a605efb997
aria <ariaserenityvt@gmail.com>
2026-03-21 22:11:33 -0400

    mounts

---
+++ hosts/x86_64-nixos/yume/mounts.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  fileSystems."/mnt/common" = {
+    device = "192.168.2.14:/mnt/d01/common";
+    fsType = "nfs";
+    options = [
+      "rw"
+      "hard"
+      "intr"
+      "nolock"
+    ];
+  };
+}
